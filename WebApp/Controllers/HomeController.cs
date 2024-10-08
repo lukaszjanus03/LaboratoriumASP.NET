f96c9fac5185726ebb69dddbe420d5d7d861611e
lukasz.janus <lukasz.janus@microsoft.wsei.edu.pl>
2024-10-08 18:17:15 +0200

    Kalkulator

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using WebApp.Models;
 
 namespace WebApp.Controllers;
@@ -11,6 +12,62 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+    }
+    
+    /*
+     * Zadanie 1 
+    * Zdefiniuj metodę z widokiem Calculator
+    * Dodaj link nawigacyjny do tej metody
+     * Zadanie 2
+     * Dodaj do kalkulatora:
+     * Operator pow, który podnosiz x do potęgi y
+     * funkcje sin, która oblicza sin(x), y jest zbędna
+     * 
+    */
+    public IActionResult Calculator(string op, double? x, double? y)
+    {
+       // var op = Request.Query["op"];
+       // var x = double.Parse(Request.Query["x"]);
+       // var y = double.Parse(Request.Query["y"]);
+       if (x is null || y is null)
+       {
+           ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze x lub y";
+           return View("CalculatorError");
+       }
+
+       if (op is null)
+       {
+           ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze x lub y";
+           return View("CalculatorError");
+       }
+       
+       switch (op)
+       {
+          case "add":
+              ViewBag.Result = x + y;
+              break;
+          case "sub":
+              ViewBag.Result = x - y;
+              break;
+          case "mul":
+              ViewBag.Result = x * y;
+              break;
+          case "div":
+              ViewBag.Result = x / y;
+              break;
+          case "pow":
+              ViewBag.Result = Math.Pow((double)x,(double)y);
+              break;
+          case "sin":
+              ViewBag.Result = Math.Sin((double)x);
+              break;
+       }
+        return View();
+    }
+    
+    public IActionResult About()
+    {
+        return View();
     }
 
     public IActionResult Index()
@@ -28,4 +85,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public enum Operator
+{
+    Add, Sub, Mul, Div , Pow, Sin
 }
